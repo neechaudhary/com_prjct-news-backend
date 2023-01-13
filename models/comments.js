6314734f827e044facc6b10f792689e661cdd157
neechaudhary <neerajbhumihaar@gmail.com>
2023-01-13 17:33:34 +0530

    updated comments

--- models/comments.js
+++ models/comments.js
@@ -1,24 +1,40 @@
+//comment schemas
+
 const mongoose = require("mongoose")
 
-const commentsSchema= mongoose.Schema({
-    userid :{
+const commentsSchema = mongoose.Schema({
+    userid: {
         type: String,
     },
-    postid : {
-        type : Number,
+    postid: {
+        type: Number,
     },
-    comments : {
-        type : String, 
+    comments: {
+        type: String,
     },
-    nestedcomments : [
-        {
-            userid:{type:String,required:true},
-            realuser:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
-            comments:{type:String,required:true},
-            date:{type:Date,required:true},
-            uniqueId:{type:String,required:true},   
-        }
-    ]
-},{timestamps:true})
+    nestedcomments: [{
+        userid: {
+            type: String,
+            required: true
+        },
+        realuser: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
 
-module.exports= mongoose.model("comments", commentsSchema) 
+        },
+        comments: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        },
+        comment_id: {
+            type: String,
+            required: true
+        },
+    }]
+}, { timestamps: true })
+
+module.exports = mongoose.model("comments", commentsSchema)
--- routes/comments.js
+++ routes/comments.js
@@ -1,8 +1,10 @@
+// comment routes
+
 const express = require("express");
 const router = express.Router();
-const comments = require("../models/comments");
+const comments = require("../models/comments_neeraj_schema");
 require("dotenv").config();
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
 //jwt
 const jwt = require("jsonwebtoken");
@@ -11,29 +13,30 @@ const jwt = require("jsonwebtoken");
 
 
 //do comment here//comment will be posted on some post. add post_id to the route
-router.post("/:id", async (req, res) => { 
+router.post("/:id", async(req, res) => {
 
     const post_id = req.params.id;
     //get token
     const token = req.cookies.auth_token || req.body.token || req.headers["x-auth-token"];
 
     //fetch user  from token
-    const user = jwt.verify(token, process.env.JWT_SECRET, {  
+    const user = jwt.verify(token, process.env.JWT_SECRET, {
         algorithm: "HS256",
     });
-    
+
     //get user id 
     if (!user) {
         return res.json(false);
     }
-    console.log(user)//console information stored in the token
+    console.log(user) //console information stored in the token
+    console.log(user.id)
 
     //comment here
     const comment = new comments({
-        userid: user.userid,
+        userid: user.id,
         postid: post_id,
-        comment: req.body.comment
-    }) 
+        comments: req.body.comments
+    })
     await comment.save((err, result) => {
         if (err) {
             res.status(200).send(err)
@@ -51,31 +54,31 @@ router.post("/:id", async (req, res) => {
 })
 
 //get all comments
-router.get("/:id", async (req, res) => { 
+router.get("/:id", async(req, res) => {
     const post_id = req.params.id;
     await comments
         .find({
             postid: post_id
         })
-        .populate("userid")
+        .populate({ userid }, { username })
         .exec((err, result) => {
             if (err) {
-                res.status(200).send 
+                res.status(200).send
                 console.log(err)
             } else {
-                res.status(200).send({  
+                res.status(200).send({
                     message: "comments",
                     result
                 })
-                console.log(result); 
+                console.log(result);
             }
-        })  
+        })
 })
 
 //update comment
-router.put("/:id", async (req, res) => {
+router.put("/:id", async(req, res) => {
     const comment_id = req.params.id;
-    const comment_update= await comments.findById(comment_id);
+    const comment_update = await comments.findById(comment_id);
 
     //get token
     const token = req.cookies.auth_token || req.body.token || req.headers["x-auth-token"];
@@ -92,145 +95,96 @@ router.put("/:id", async (req, res) => {
 
     //check user is owner of comment
     if (user.id == comment_update.userid) {
-        const comment_update= await comments.findByIdAndUpdate(comment_id);
+        const comment_update = await comments.findByIdAndUpdate(comment_id);
 
-        if(!comment_update){
+        if (!comment_update) {
             res.status(200).send({
                 message: "comment not found",
             })
-        }else{
-            comment_update.comments= req.body.comments;
+        } else {
+            comment_update.comments = req.body.comments;
             await comment_update.save();
-              res.status(200).send({
+            res.status(200).send({
                 message: "comment updated",
-                status:"success"
+                status: "success"
             })
-         }       
-    }else{
-      if(err){
-        res.status(500).json({message:error.message});
-      }
+        }
+    } else {
+        if (err) {
+            res.status(500).json({ message: error.message });
+        }
     }
 
 
-    
+
 })
 
 //delete comment
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async(req, res) => {
     const comment_id = req.params.id;
-    if(mongoose.Types.ObjectId.isValid(comment_id)){
-    const comments_delete= await comments.findByIdAndDelete(comment_id)
+    if (mongoose.Types.ObjectId.isValid(comment_id)) {
+        const comments_delete = await comments.findByIdAndDelete(comment_id)
 
-    if (!comments_delete) {
-        return res.status(404).send({
-            message: "comment not found",  
-            status:"error"
+        if (!comments_delete) {
+            return res.status(404).send({
+                message: "comment not found",
+                status: "error"
+            })
+        }
+        await comments_delete.remove();
+        res.status(200).send({
+            message: "comment deleted",
+            status: "success"
         })
+        console.log(comments_delete);
+
     }
-    await comments_delete.remove(); 
-    res.status(200).send({   
-        message: "comment deleted",
-        status:"success"
-    })
-    console.log(comments_delete);
-  
-   }
 });
 
 //replies on comments ===============>nested comments=====================================================
-router.post("/reply/:comment_id/:user_id", async (req, res) => {
+router.post("/reply/:comment_id/:user_id", async(req, res) => {
     // const user_id = req.params.user_id;
 
     try {
 
-       //get token
-    const token = req.cookies.auth_token || req.body.token || req.headers["x-auth-token"];
+        //get token
+        const token = req.cookies.auth_token || req.body.token || req.headers["x-auth-token"];
 
-    //fetch user  from token
-    const user = jwt.verify(token, process.env.JWT_SECRET, {  
-        algorithm: "HS256",
-    });
+        //fetch user  from token
+        const user = jwt.verify(token, process.env.JWT_SECRET, {
+            algorithm: "HS256",
+        });
 
-    //get user id
-    if (!user) {
-        return res.json({message:"You are not logged in", status:false});
-    }
-    console.log(user)//console information stored in the token
+        //get user id
+        if (!user) {
+            return res.json({ message: "You are not logged in", status: false });
+        }
+        console.log(user) //console information stored in the token
 
-    if(user.userid != req.params.user_id) 
-        return res.status(404).send({message:"please login to reply", status: false})
+        if (user.id != req.params.user_id)
+            return res.status(404).send({ message: "please login to reply", status: false })
 
-        let data= {
+        let data = {
             comment_id: req.params.comment_id,
             userid: req.params.user_id,
             realuser: req.body.realuser,
             comments: req.body.comments,
             Date: new Date()
         };
-        const pushReplies = await comments.updateOne( 
-            { _id: req.params.comment_id },
-            { $push: { nestedcomments: data } },   
-            { new: true }
-          )
+        const pushReplies = await comments.updateOne({ _id: req.params.comment_id }, { $push: { nestedcomments: data } }, { new: true })
             .lean()
             .exec();
-            console.log(pushReplies)
-          return res.status(200).send({ message: "Success, The reply has been posted", status: true });
-        
-     
-   }catch (error) {
-    return res.status(500).send({message:error.message, status: false}) 
-  }
-})
+        console.log(pushReplies)
+        return res.status(200).send({ message: "Success, The reply has been posted", status: true });
 
-//deleting replies
-router.delete("/reply/:comment_id/:nestedcomment_id", async (req, res) => { 
-    try {
-        //get token
-        const token = req.cookies.auth_token || req.body.token || req.headers["x-auth-token"];
 
-        //fetch user  from token
-        const user = jwt.verify(token, process.env.JWT_SECRET, {  
-            algorithm: "HS256",
-        });
-
-        //get user id
-        if (!user) {
-            return res.json({message:"You are not logged in", status:false});
-        }
-
-        const comment = await comments.findById(req.params.comment_id);
-        if (!comment) {
-            return res.status(404).send({message:"comment not found", status: false})
-        }
-        const nestedcomment = comment.nestedcomments.find(
-            (nestedcomment) => nestedcomment._id == req.params.nestedcomment_id
-        );
-        if (!nestedcomment) {
-            return res.status(404).send({message:"reply not found", status: false})
-        }
-        console.log(nestedcomment.userid)
-        console.log(user.id)
-
-        if (nestedcomment.userid != user.userid) {
-            return res.status(404).send({message:"you are not allowed to delete this reply", status: false})
-        }
-        const removeNestedcomment = await comments.updateOne(
-            { _id: req.params.comment_id },
-            { $pull: { nestedcomments: { _id: req.params.nestedcomment_id } } }, 
-            { new: true }
-        ) 
-            .lean()
-            .exec();
-        return res.status(200).send({ message: "Success, The reply has been deletd", status: true });
     } catch (error) {
-        return res.status(500).send({message:error.message, status: false})
+        return res.status(500).send({ message: error.message, status: false })
     }
 })
 
-//edit replies
-router.put("/reply/:comment_id/:nestedcomment_id", async (req, res) => {
+//deleting replies
+router.delete("/reply/:comment_id/:nestedcomment_id", async(req, res) => {
     try {
         //get token
         const token = req.cookies.auth_token || req.body.token || req.headers["x-auth-token"];
@@ -241,31 +195,68 @@ router.put("/reply/:comment_id/:nestedcomment_id", async (req, res) => {
         });
 
         //get user id
-        if (!user) { 
-            return res.json({message:"You are not logged in", status:false});
+        if (!user) {
+            return res.json({ message: "You are not logged in", status: false });
         }
 
         const comment = await comments.findById(req.params.comment_id);
         if (!comment) {
-            return res.status(404).send({message:"comment not found", status: false})
-        }   
+            return res.status(404).send({ message: "comment not found", status: false })
+        }
+        const nestedcomment = comment.nestedcomments.find(
+            (nestedcomment) => nestedcomment._id == req.params.nestedcomment_id
+        );
+        if (!nestedcomment) {
+            return res.status(404).send({ message: "reply not found", status: false })
+        }
+        console.log(nestedcomment.userid)
+        console.log(user.id)
+
+        if (nestedcomment.userid != user.userid) {
+            return res.status(404).send({ message: "you are not allowed to delete this reply", status: false })
+        }
+        const removeNestedcomment = await comments.updateOne({ _id: req.params.comment_id }, { $pull: { nestedcomments: { _id: req.params.nestedcomment_id } } }, { new: true })
+            .lean()
+            .exec();
+        return res.status(200).send({ message: "Success, The reply has been deletd", status: true });
+    } catch (error) {
+        return res.status(500).send({ message: error.message, status: false })
+    }
+})
+
+//edit replies
+router.put("/reply/:comment_id/:nestedcomment_id", async(req, res) => {
+    try {
+        //get token
+        const token = req.cookies.auth_token || req.body.token || req.headers["x-auth-token"];
+
+        //fetch user  from token
+        const user = jwt.verify(token, process.env.JWT_SECRET, {
+            algorithm: "HS256",
+        });
+
+        //get user id
+        if (!user) {
+            return res.json({ message: "You are not logged in", status: false });
+        }
+
+        const comment = await comments.findById(req.params.comment_id);
+        if (!comment) {
+            return res.status(404).send({ message: "comment not found", status: false })
+        }
         const nestedcomment = comment.nestedcomments.find((nestedcomment) => nestedcomment._id == req.params.nestedcomment_id);
         if (!nestedcomment) {
-            return res.status(404).send({message:"reply not found", status: false})
+            return res.status(404).send({ message: "reply not found", status: false })
         }
         if (nestedcomment.userid != user.userid) {
-            return res.status(404).send({message:"you are not allowed to edit this reply", status: false})
+            return res.status(404).send({ message: "you are not allowed to edit this reply", status: false })
         }
-        const updateNestedcomment = await comments.updateOne(
-            { _id: req.params.comment_id, "nestedcomments._id": req.params.nestedcomment_id },
-            { $set: { "nestedcomments.$.comments": req.body.comments } },
-            { new: true }
-        )
+        const updateNestedcomment = await comments.updateOne({ _id: req.params.comment_id, "nestedcomments._id": req.params.nestedcomment_id }, { $set: { "nestedcomments.$.comments": req.body.comments } }, { new: true })
             .lean()
             .exec();
         return res.status(200).send({ message: "Success,Your reply has been edited.", status: true });
     } catch (error) {
-        return res.status(500).send({message:error.message, status: false})
+        return res.status(500).send({ message: error.message, status: false })
     }
 })
 
